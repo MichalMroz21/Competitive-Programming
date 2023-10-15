7d89d86aea0238e97003108d6cac5da3ccc29401
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2023-10-15 23:26:39 +0200

    +2

---
+++ Answers/1777.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT product_id,
+SUM(IF(store = 'store1', price, null)) AS store1,
+SUM(IF(store = 'store2', price, null)) AS store2,
+SUM(IF(store = 'store3', price, null)) AS store3
+FROM Products
+GROUP BY product_id
