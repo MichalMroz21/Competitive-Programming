2aada6d232a712eda36a00bee5e801e0cca9f6eb
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2023-11-10 20:36:21 +0100

    better

--- Answers/2923.cpp
+++ Answers/2923.cpp
@@ -2,22 +2,15 @@ class Solution {
 public:
     int findChampion(vector<vector<int>>& grid) {
         
-        unordered_map<int, bool> isWorse{};
-
         int n = grid.size();
 
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < grid[i].size(); j++){
-                if(i == j) continue;
-
-                if(grid[i][j]){
-                    isWorse[j] = true;
-                }
+        for(int j = 0; j < n; j++){
+            int cnt{};
+            for(int i = 0; i < n; i++){
+                cnt += grid[i][j];
             }
-        }
 
-        for(int i = 0; i < n; i++){
-            if(isWorse[i] == false) return i;
+            if(cnt == 0) return j;
         }
 
         return -1;
