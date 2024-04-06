d443d386e5b881f895f45bde072955f15079ae98
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2024-04-07 00:11:43 +0200

    ~1

--- Leetcode/3064.cpp
+++ Leetcode/3064.cpp
@@ -6,13 +6,24 @@
 class Solution {
 public:
     int findNumber() {
-        int ans = 0;
-        for (int i = 0; i < 31; ++i) {
-            if (commonSetBits(1 << i)) {
-                ans |= 1 << i;
+
+        int mask1 = INT_MAX, mask2 = 1,
+            diff = commonSetBits(mask1);
+
+        bitset<sizeof(int) * CHAR_BIT> ans{};
+
+        for(int i = 0; i < sizeof(int) * CHAR_BIT; i++){
+
+            mask1 ^= mask2;
+            mask2 <<= 1;
+
+            if(commonSetBits(mask1) < diff){
+                ans[i] = 1;
             }
+
+            mask1 = INT_MAX;
         }
 
-        return ans;
+        return ans.to_ulong();
     }
 };
