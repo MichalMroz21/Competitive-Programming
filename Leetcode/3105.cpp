06ccddfc7097070727f4a5482faad5429f5445ab
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2024-04-09 00:01:47 +0200

    ~`

--- Leetcode/3105.cpp
+++ Leetcode/3105.cpp
@@ -1,9 +1,27 @@
-int longestMonotonicSubarray(const vector<int>& nums) {
-        int ans = 1;
-        for (int inc = 1, dec = 1, i = 1; i < ssize(nums); ++i) {
-            inc = nums[i - 1] <= nums[i] ? 1 : inc + 1;
-            dec = nums[i - 1] >= nums[i] ? 1 : dec + 1;
-            ans = max({ans, inc, dec});
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        int ans = 1, curr = 1;
+
+        bool increasing = true;
+
+        if(nums.empty()) return 0;
+
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i - 1] > nums[i]){
+                if(increasing) curr = 1;
+                increasing = false;
+            }
+            else if(nums[i - 1] < nums[i]){
+                if(!increasing) curr = 1;
+                increasing = true;
+            }
+            else curr = 0;
+
+            curr++;
+            ans = max(ans, curr);
         }
+
         return ans;
     }
+};
