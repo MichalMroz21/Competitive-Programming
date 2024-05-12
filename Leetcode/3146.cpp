4a66c452edd913cf008eb4d54b23dc9234f3aeba
MichalMroz <125133223+MichalMroz21@users.noreply.github.com>
2024-05-12 23:58:17 +0200

    +1

---
+++ Leetcode/3146.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findPermutationDifference(string s, string t) {
+        unordered_map<char, int> mp{};
+
+        int ans{};
+
+        for(int i = 0; i < s.size(); i++) mp[s[i]] = i;
+        for(int i = 0; i < t.size(); i++) ans += abs(i - mp[t[i]]);
+
+        return ans;
+    }
+};
